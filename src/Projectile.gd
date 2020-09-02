b7d116d58aa5e13a766e2cf5f0a6ecee485ef91b
Ethan Bowers <ejbowers@bsu.edu>
2020-09-02 11:54:14 -0400

    added movement to projectile

--- src/Projectile.gd
+++ src/Projectile.gd
@@ -1,10 +1,12 @@
 extends RigidBody2D
 
+export var strength : float = 500
 
-
-func _ready():
-	pass # Replace with function body.
+func _process(delta):
+	if Input.is_action_just_pressed("launch"):
+		var direction = Vector2(1,-1).normalized()
+		var velocity= direction * strength
+		apply_impulse(Vector2.ZERO, velocity)
 
 func _draw():
 	draw_circle(Vector2.ZERO, $CollisionShape2D.shape.radius, Color.black)
-	
